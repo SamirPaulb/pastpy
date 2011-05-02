c8a9c9025849e75a0a6354f246fa0a87437b3956
Samir Paul <samir.paul.personal@gmail.com>
2011-05-02 12:12:00 +0530

    py

--- past.py
+++ past.py
@@ -6,9 +6,10 @@ git commit --date='2010-02-02 12:12:00' -m "past.py"
 '''
 
 ''' 
-git commit --date='2010-05-02 12:12:00' -m "py"
+git commit --date='2011-05-02 12:12:00' -m "py"
 
 '''
+
 
 
 ''' 
